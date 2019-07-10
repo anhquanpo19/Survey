e7870084500b233195e1a487abb9ba7b97c71cdd
Danny <maxquanmin@gmail.com>
2019-07-10 07:40:42 +0700

    Create Class_userModels.cs

---
+++ WebSurvey1/WebSurvey1/Models/Class_userModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSurvey1.Models
+{
+    public class Class_userModels
+    {
+        public int Class_ID { get; set; }
+        public int User_ID { get; set; }
+        public bool status { get; set; }
+
+        public virtual Class Class { get; set; }
+        public virtual User User { get; set; }
+    }
+}
