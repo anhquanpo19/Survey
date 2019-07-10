4366a048d73d314221c4b7724d16c7c3023c0e87
Danny <maxquanmin@gmail.com>
2019-07-10 07:41:42 +0700

    Create User_metaModels.cs

---
+++ WebSurvey1/WebSurvey1/Models/User_metaModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSurvey1.Models
+{
+    public class User_metaModels
+    {
+        public int usermeta_ID { get; set; }
+        public int user_ID { get; set; }
+        public string meta_key { get; set; }
+        public string meta_value { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
