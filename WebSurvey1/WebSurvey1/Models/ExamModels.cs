9b64a6fa7201190c1b9758b435f21b4391d65aab
Danny <maxquanmin@gmail.com>
2019-07-10 07:40:58 +0700

    Create ExamModels.cs

---
+++ WebSurvey1/WebSurvey1/Models/ExamModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSurvey1.Models
+{
+    public class ExamModels
+    {
+        public int exm_ID { get; set; }
+        public int survey_ID { get; set; }
+        public int user_ID { get; set; }
+        public int number_question { get; set; }
+        public double total_score { get; set; }
+        public System.TimeSpan time_score { get; set; }
+
+        public virtual Survey Survey { get; set; }
+        public virtual User User { get; set; }
+    }
+}
