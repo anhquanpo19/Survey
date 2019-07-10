d9c6a159333ce92141e069a0c1788d1c9b31feeb
Danny <maxquanmin@gmail.com>
2019-07-10 11:45:35 +0700

    Update UserModels.cs

--- WebSurvey1/WebSurvey1/Models/UserModels.cs
+++ WebSurvey1/WebSurvey1/Models/UserModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -18,11 +19,30 @@ namespace WebSurvey1.Models
 
 
         public int ID { get; set; }
+
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "You need give us your name or roll Id")]
         public string user_name { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You need give us your Password")]
+        [DataType(DataType.Password)]
         public string user_password { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "You need give us your Email")]
+        [DataType(DataType.EmailAddress)]
         public string user_email { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Please give us your Name!")]
+        
         public string user_nicename { get; set; }
-        public bool user_status { get; set; }
+         
+
+        public bool User_status { get; set; }
+
+
         public string display_name { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
@@ -34,5 +54,4 @@ namespace WebSurvey1.Models
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<User_metaModels> User_metaModel { get; set; }
     }
-}
 }
