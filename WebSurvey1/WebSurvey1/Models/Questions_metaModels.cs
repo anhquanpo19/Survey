87fd3f5c79d86d45fb72dcc765d6e3a764ac3f4e
Danny <maxquanmin@gmail.com>
2019-07-10 07:41:26 +0700

    Create Questions_metaModels.cs

---
+++ WebSurvey1/WebSurvey1/Models/Questions_metaModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSurvey1.Models
+{
+    public class Questions_metaModels
+    {
+        public int ID { get; set; }
+        public int question_ID { get; set; }
+        public string question_key { get; set; }
+        public string question_value { get; set; }
+
+        public virtual Question Question { get; set; }
+    }
+}
