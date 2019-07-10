702a57cf59d6be9c6f15b68ecef0408da1f28e6f
Danny <maxquanmin@gmail.com>
2019-07-10 07:41:24 +0700

    Create QuestionModels.cs

---
+++ WebSurvey1/WebSurvey1/Models/QuestionModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSurvey1.Models
+{
+    public class QuestionModels
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public QuestionModels()
+        {
+            this.Questions_metaModel = new HashSet<Questions_metaModels>();
+        }
+
+        public int question_ID { get; set; }
+        public string question_Date { get; set; }
+        public string question_tile { get; set; }
+        public string question_status { get; set; }
+        public string question_type { get; set; }
+        public int question_resuit { get; set; }
+        public int survey_ID { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Questions_metaModels> Questions_metaModel { get; set; }
+        public virtual Survey Survey { get; set; }
+    }
+}
+}
